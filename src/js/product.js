3357623fcbd8444eecb521b3b0cc521171abbcc2
darvfus <romerondan123@gmail.com>
2024-09-24 10:55:25 -0600

    "change for cart"

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-
-const dataSource = new ProductData("tents");
-
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+ 
+const dataSource = new ProductData('tents');
+ 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Verificar si ya hay productos en el carrito
+  let cart = getLocalStorage('so-cart') || [];
+  // Agregar el nuevo producto al carrito
+  cart.push(product);
+  // Actualizar el localStorage con el carrito actualizado
+  setLocalStorage('so-cart', cart);
 }
+ 
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
-
+ 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
