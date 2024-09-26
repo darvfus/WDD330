fd1ded98132d011068a126157a3febb9d68683fd
darvfus <romerondan123@gmail.com>
2024-09-26 14:13:59 -0600

    personal1

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,10 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
- 
-const dataSource = new ProductData('tents');
- 
-function addProductToCart(product) {
-  // Verificar si ya hay productos en el carrito
-  let cart = getLocalStorage('so-cart') || [];
-  // Agregar el nuevo producto al carrito
-  cart.push(product);
-  // Actualizar el localStorage con el carrito actualizado
-  setLocalStorage('so-cart', cart);
-}
- 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
- 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// Imports
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
