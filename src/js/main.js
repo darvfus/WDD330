64302987eaab73e1f267bafeea4d694a2d2ffa8c
PurpleCre <pgonese6@gmail.com>
2024-10-11 18:20:51 +0200

    test

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,6 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-const tentData = ProductData("tents");
+const tentData = new ProductData("tents");
+
 loadHeaderFooter();
