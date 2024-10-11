e5c0e0c53ec6720deb78afb64064297e89f54a52
PurpleCre <pgonese6@gmail.com>
2024-10-11 19:03:30 +0200

    fix: product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,5 @@
 // Imports
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -12,5 +11,3 @@ console.log(productId);
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-loadHeaderFooter();
