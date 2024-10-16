e63be5e253cf571e74d893f7fe1584ebada49277
PurpleCre <pgonese6@gmail.com>
2024-10-16 15:30:37 +0200

    main-fix

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,41 +1,41 @@
 class Cart {
-    constructor() {
-      this.items = JSON.parse(localStorage.getItem('cart')) || [];
-    }
-  
-    addItem(product) {
-      this.items.push(product);
-      this.save();
-    }
-  
-    removeItem(productId) {
-      this.items = this.items.filter(item => item.Id !== productId);
-      this.save();
-    }
-  
-    getItems() {
-      return this.items;
-    }
-  
-    save() {
-      localStorage.setItem('cart', JSON.stringify(this.items));
-    }
+  constructor() {
+    this.items = JSON.parse(localStorage.getItem("cart")) || [];
   }
-  
-  export default new Cart();
-  import cart from './cart.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const cartListElement = document.querySelector('.cart-list');
-    const items = cart.getItems();
+  addItem(product) {
+    this.items.push(product);
+    this.save();
+  }
 
-    if (items.length === 0) {
-        cartListElement.innerHTML = '<li>Your cart is empty.</li>';
-    } else {
-        items.forEach(item => {
-            const listItem = document.createElement('li');
-            listItem.textContent = `${item.NameWithoutBrand} - $${item.FinalPrice}`;
-            cartListElement.appendChild(listItem);
-        });
-    }
+  removeItem(productId) {
+    this.items = this.items.filter((item) => item.Id !== productId);
+    this.save();
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  save() {
+    localStorage.setItem("cart", JSON.stringify(this.items));
+  }
+}
+
+export default new Cart();
+import cart from "./cart.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cartListElement = document.querySelector(".cart-list");
+  const items = cart.getItems();
+
+  if (items.length === 0) {
+    cartListElement.innerHTML = "<li>Your cart is empty.</li>";
+  } else {
+    items.forEach((item) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${item.NameWithoutBrand} - $${item.FinalPrice}`;
+      cartListElement.appendChild(listItem);
+    });
+  }
 });
